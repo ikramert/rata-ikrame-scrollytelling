3203cd6209abbefc3279a2a0ec4f2d471b7a6f67
ikramert <112107968+ikramert@users.noreply.github.com>
2024-04-29 14:23:26 -0400

    p2.2

--- site-scrolly/script/script.js
+++ site-scrolly/script/script.js
@@ -26,3 +26,32 @@ function defilement() {
 
 //écouteur d'événement
 window.addEventListener("scroll", defilement);
+
+// Chapitre 1 svg anim
+gsap.registerPlugin(ScrollTrigger);
+
+// Timeline d'animation pour CHAPITRE 1
+const timeline1 = gsap
+  .timeline({ repeat: -1, yoyo: true })
+  .to(".dauphinimg", {
+    y: "10vh",
+    x: "50vw",
+    duration: 10,
+    ease: "power1.inOut",
+  })
+  .fromTo(
+    ".poissonrouge",
+    { x: "100%", y: "20vh" },
+    { x: "-100%", y: "20vh", duration: 10, ease: "none" },
+    "<"
+  )
+  // Animation pour CHAPITRE 2
+  .fromTo(
+    ".poissoncoloreimg",
+    { x: "100%", y: "0%" },
+    { x: "-100%", y: "0%", duration: 10, ease: "none" },
+    "<"
+  )
+  .to(".poissoncoloreimg", { y: "50%", duration: 5, ease: "power1.inOut" }, "<")
+  .to(".poissoncoloreimg", { y: "0%", duration: 5, ease: "power1.inOut" })
+  .to(".poissoncoloreimg", { opacity: 1, duration: 2 }, "<");
